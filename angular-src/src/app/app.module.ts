4e41bfdba4394d73a1295e92b4837042b5858f0b
Tomaz Jejcic <tomazjejcic@gmail.com>
2017-09-23 15:54:36 +0800

    build(components) register build(services) validate

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
+import { FlashMessagesModule } from 'angular2-flash-messages';
+
+// Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
@@ -9,6 +13,9 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
+
+// Services
+import { ValidateService } from './services/validate.service';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
@@ -30,9 +37,13 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule
   ],
-  providers: [],
+  providers: [
+    ValidateService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+    name: String;
+    username: String;
+    email: String;
+    password: String;
 
-  ngOnInit() {
-  }
+    constructor(
+
+        private validateService: ValidateService,
+        private flashMessage: FlashMessagesService
+    ) {
+
+    }
+
+    ngOnInit() {
+
+    }
+
+    onRegisterSubmit() {
+
+        const user = {
+            name: this.name,
+            username: this.username,
+            email: this.email,
+            password: this.password
+        }
+
+        // Required Fields
+        if (!this.validateService.validateRegister(user)) {
+            this.flashMessage.show('Please fill in all fields', {cssClass: 'alert-danger', timeout: 3000});
+            return false;
+        }
+
+        // Validate Email
+        if (!this.validateService.validateEmail(user.email)) {
+            this.flashMessage.show('Please use a valid email', {cssClass: 'alert-danger', timeout: 3000});
+            return false;
+        }
+    }
 
 }
