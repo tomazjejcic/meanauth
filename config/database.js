9a96c347377b332af99752a7e4c81130b9e97be4
Tomaz Jejcic <tomazjejcic@gmail.com>
2017-09-26 16:48:46 +0800

    add(config) mlab linked mongo db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/meanauth',
+    // database: 'mongodb://localhost:27017/meanauth', // local database
+    database: 'mongodb://toto:123@ds123614.mlab.com:23614/meanauthapp', // mlab mongo, heroku
     secret: 'yoursecret'
 }
